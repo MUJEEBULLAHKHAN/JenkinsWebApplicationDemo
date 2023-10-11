50cdb0a90beafe6bd4a896cc506572398cfa8e0d
sumair1983 <sumair29@gmail.com>
2023-10-11 16:58:53 +0300

    Update WeatherForecastController.cs

--- JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
+++ JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace JenkinsWebApplicationDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Racing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
